ed588a473dd175eea6b3d4522c9bcdc8d3bf13bf
Timothy Shiu <timoweave@gmail.com>
2017-11-23 20:21:10 -0800

    add the option selected attribute

--- src/Book.js
+++ src/Book.js
@@ -15,7 +15,7 @@ export type State = {
 class Book extends Component<Props, State> {
 
     render(): Element<'div'> {
-        const {title, authors, imageLinks} = this.props.book;
+        const {title, authors, imageLinks, shelf} = this.props.book;
         const img = {
             backgroundImage: `url(${imageLinks.smallThumbnail})`,
         };
@@ -27,7 +27,7 @@ class Book extends Component<Props, State> {
                 <div className="book_shelf_changer">
                   <select>
                     {status_list.map((opt: Status): Element<'option'> => (
-                        <option key={opt.value} value={opt.value}>
+                        <option key={opt.value} value={opt.value} selected={opt.value === shelf}>
                           {opt.description}
                         </option>
                     ))}
